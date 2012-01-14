08ff0fe92a63be3bbd9ba99dd5c064c34a118f31
Kaique Silva <kaique.developer@gmail.com>
2012-01-14 01:12:50 -0200

    Put information in the table

--- controllers/default.py
+++ controllers/default.py
@@ -14,17 +14,4 @@ def call():
 
 @auth.requires_signature()
 def data():
-    """
-    http://..../[app]/default/data/tables
-    http://..../[app]/default/data/create/[table]
-    http://..../[app]/default/data/read/[table]/[id]
-    http://..../[app]/default/data/update/[table]/[id]
-    http://..../[app]/default/data/delete/[table]/[id]
-    http://..../[app]/default/data/select/[table]
-    http://..../[app]/default/data/search/[table]
-    but URLs bust be signed, i.e. linked with
-      A('table',_href=URL('data/tables',user_signature=True))
-    or with the signed load operator
-      LOAD('default','data.load',args='tables',ajax=True,user_signature=True)
-    """
     return dict(form=crud())
